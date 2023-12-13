f32ab392052ff67c6bb53797f1197c2cb290ddfd
jesus jimenez <jesus@jesuss-MBP.fibertel.com.ar>
2023-12-13 00:10:02 -0300

    initial commit?

---
+++ src/routes/+page.svelte
@@ -0,0 +1,35 @@
+<script>
+  /** @type {string[]} */
+  let todos = []
+
+  /** @type {string} */
+  let description = ''
+
+  function addTodoAction() {
+    if (description.length) {
+      todos = [...todos, description]
+    }
+  }
+</script>
+
+<h1>Boring Todos</h1>
+<p>Todo app coded with svelte</p>
+
+<div>
+  <form action="">
+    <label for="desc">Add a Todo: </label>
+    <input
+      type="text"
+      required
+      placeholder="...your todo here"
+      id="desc"
+      bind:value={description}
+    />
+    <button on:click={addTodoAction}>+</button>
+  </form>
+  <ul>
+    {#each todos as todo}
+      <li>{todo}</li>
+    {/each}
+  </ul>
+</div>
